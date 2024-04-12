86803da2fbe192bd7c99a8e9b8f18e9495084573
Vaibhav Gutthe <gutthevaibhav0@gmail.com>
2024-04-12 12:31:28 +0530

    first commit

---
+++ files/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php require 'nav.html' ?>
+    <p>&nbsp; KEEP VISITORS COMING BACK FOR MORE WITH ENGAGING AND RELEVANT CONTENT</p><p><br /></p><p>Most visitors to a website scan through the content as opposed to reading it line by line. Our website content writers are well versed with this fact and are experts at keeping the structure and format of the articles, blogs or webpages such that online readers find it convenient to go through them. We deliver content that readers find engaging, relevant, easily comprehensible and in line with your best expectations.</p>
+    <?php require 'footer.html' ?>
+</body> 
+</html>
---
+++ ind.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require 'header.html'
+    
+    ?>
+    <p>can write hear you code</p>
+    <?php require 'footer.html'
+    ?>
+</body>
+</html>
